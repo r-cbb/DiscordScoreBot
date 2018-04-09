88559cc70924d313fdcb7051b6a50ea5fa850b7b
cinciforthewin <tangemandavid@gmail.com>
2018-04-09 17:41:44 -0400

    Removed Extra Testing Functions
    Removed Eight Ball
    Removed Square
    Removed bitcoin

--- mybot.py
+++ mybot.py
@@ -99,41 +99,10 @@ async def schedule(*,team:str):
 		print(str(e))
 		await client.say("Error Occured")
 		
-@client.command(name='8ball',
-                description="Answers a yes/no question.",
-                brief="Answers from the beyond.",
-                aliases=['eight_ball', 'eightball', '8-ball'],
-                pass_context=True)
-async def eight_ball(context):
-    possible_responses = [
-        'That is a resounding no',
-        'It is not looking likely',
-        'Too hard to tell',
-        'It is quite possible',
-        'Definitely',
-    ]
-    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
-
-
-@client.command()
-async def square(number):
-    squared_value = int(number) * int(number)
-    await client.say(str(number) + " squared is " + str(squared_value))
-
-
 @client.event
 async def on_ready():
     await client.change_presence(game=Game(name="Basketball"))
     print("Logged in as " + client.user.name)
-
-
-@client.command()
-async def bitcoin():
-    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
-    response = requests.get(url)
-    value = response.json()['bpi']['USD']['rate']
-    await client.say("Bitcoin price is: $" + value)
-
 
 async def list_servers():
     await client.wait_until_ready()
